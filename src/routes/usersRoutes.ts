65fff0c94671c4374284d97e241ab1251bc59046
camilo.matteo <camilo.matteo@babelgroup.com>
2024-06-19 17:29:42 -0400

    :bug: update gitignore

--- src/routes/usersRoutes.ts
+++ src/routes/usersRoutes.ts
@@ -1,10 +1,15 @@
-import express from 'express'
-import { deleteUser, getAllUsers, getUser, updateUser } from '../controllers/userController'
-import { verifyToken } from '../middlewares/verifyToken'
+import express from "express";
+import {
+  deleteUser,
+  getAllUsers,
+  getUser,
+  updateUser,
+} from "../controllers/userController";
+import { verifyToken } from "../middlewares/verifyToken";
 
 export default (router: express.Router) => {
-  router.get('/users', verifyToken, getAllUsers)
-  router.get('/users/:id', verifyToken, getUser)
-  router.delete('/users/:id', verifyToken, deleteUser)
-  router.patch('/users/:id', verifyToken, updateUser)
-}
+  router.get("/users", verifyToken, getAllUsers);
+  router.get("/users/:id", verifyToken, getUser);
+  router.delete("/users/:id", verifyToken, deleteUser);
+  router.patch("/users/:id", verifyToken, updateUser);
+};
